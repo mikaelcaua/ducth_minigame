fb7e9354c1fbf00428242a64c93b80b66c125241
mikaelcaua <mikaelcauasilva1@gmail.com>
2025-01-10 08:21:05 -0300

    feat: add number and card suit in card model.

---
+++ lib/app/models/player_card_model.dart
@@ -0,0 +1,10 @@
+import 'package:ducth_minigame/app/utills/card_suit.dart';
+
+class PlayerCardModel {
+  final int number;
+  final CardSuit cardSuit;
+
+  PlayerCardModel({required this.number, required this.cardSuit});
+
+  
+}
